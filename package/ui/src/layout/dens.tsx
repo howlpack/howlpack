f102b52cbb673781c02a7d8be9fd995887bccc79
janfabian <mail@janfabian.cz>
2023-03-25 21:55:01 +0100

    feat(ui): fix build

--- package/ui/src/layout/dens.tsx
+++ package/ui/src/layout/dens.tsx
@@ -11,7 +11,11 @@ export default function Dens() {
     <ThemeProvider theme={prefersDarkMode ? darkTheme : lightTheme}>
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300&display=swap"
           rel="stylesheet"
