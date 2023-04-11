c5d873d0371aa166ba69e21593921bd2718bbf88
janfabian <mail@janfabian.cz>
2023-04-11 10:16:44 +0200

    feat(ui): howlpack social

--- package/ui/src/pages/dens-path/tlds.ts
+++ package/ui/src/pages/dens-path/tlds.ts
@@ -1,3 +1,5 @@
 export default [
   "juno1xefzvnhzyedn8wkull7szq4ezyt4pgpwyysf9pyjc7uvs3j6cjdsqu203r",
+  // howlpack::social
+  "juno169v7xyz7pvuh0ke8mfleg9988w5hcqh22gm6ruygg2ukpqg7lhkqgs0c63",
 ];
