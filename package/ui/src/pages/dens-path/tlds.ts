88a96071e4ba479b309ae9f3ee67fd1ee4499b7f
janfabian <mail@janfabian.cz>
2023-04-15 09:42:38 +0200

    feat(ui): tlds update

--- package/ui/src/pages/dens-path/tlds.ts
+++ package/ui/src/pages/dens-path/tlds.ts
@@ -1,5 +1,5 @@
 export default [
   "juno1zswtyuc3er52j0768lvetg7dx5nrgp933h3982c7lsegaafz6pfqj66yc0",
   // howlpack::social
-  "juno169v7xyz7pvuh0ke8mfleg9988w5hcqh22gm6ruygg2ukpqg7lhkqgs0c63",
+  // "juno169v7xyz7pvuh0ke8mfleg9988w5hcqh22gm6ruygg2ukpqg7lhkqgs0c63",
 ];
